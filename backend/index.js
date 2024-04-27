e31e3ef7b1a5d13652c4caa0684c7b9a34dda3df
ayan <mohdayanrehman07@gmail.com>
2024-04-27 17:53:23 +0530

    Make-userModel-and-userSignin-userSignUp(file)

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,12 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const connectDB = require("./config/db");
+const router = require("./routes/index");
 
 const app = express();
 app.use(cors());
+
+app.use("/api", router);
 
 const PORT = 4000 || process.env.PORT;
 
