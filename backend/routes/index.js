2cffff908418f50b124189bb7a2aa7fb965ddc9f
ayan <mohdayanrehman07@gmail.com>
2024-04-28 12:03:09 +0530

    middleware-token-and-userDetails-api-and-define-index.js

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const userSignUpController = require("../controller/userSignUp");
 const userSignInController = require("../controller/userSignin");
+const userDetailsController = require("../controller/userDetails");
+const authToken = require("../middleware/authToken");
 
 const router = express.Router();
 
 router.post("/signup", userSignUpController);
 router.post("/signin", userSignInController);
+router.get("/user-details", authToken, userDetailsController);
 
 module.exports = router;
