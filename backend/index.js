899bbb5757497ab1b4bfa9f2c0424a3b2f20f267
ayan <mohdayanrehman07@gmail.com>
2024-04-28 10:55:53 +0530

    Some-changes-cookie-parser-install-and-cors-issue-solve

--- backend/index.js
+++ backend/index.js
@@ -3,10 +3,17 @@ const cors = require("cors");
 require("dotenv").config();
 const connectDB = require("./config/db");
 const router = require("./routes/index");
+const cookieParser = require("cookie-parser");
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
+);
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api", router);
 
