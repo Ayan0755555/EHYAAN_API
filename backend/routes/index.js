a954c063aacd1ec123759a8ca50e2cdb065b0b91
ayan <mohdayanrehman07@gmail.com>
2024-04-28 10:03:57 +0530

    jsonwebtoken-install-and-loginApi-with-define-routes-token-secreat-key

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const userSignUpController = require("../controller/userSignUp");
+const userSignInController = require("../controller/userSignin");
 
 const router = express.Router();
 
 router.post("/signup", userSignUpController);
+router.post("/signin", userSignInController);
 
 module.exports = router;
